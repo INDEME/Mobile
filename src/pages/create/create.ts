166afd81e28653deaffc935119e2589146974cb0
Ulises <ulisesmc1006@gmail.com>
2018-03-09 19:24:18 -0600

    frontend creacion encuestas

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
+import { AuthSevice } from '../../services/auth/auth';
+
 
 /**
  * Generated class for the CreatePage page.
@@ -15,11 +19,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreatePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  IdentificadorUsuario: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthSevice, public http:Http) {
+    this.IdentificadorUsuario = this.auth.idUsuario;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreatePage');
+    
   }
 
+  OnSave(){
+    this.http.post('https://apex.oracle.com/pls/apex/indeme/INpolls/', {
+      'id': this.IdentificadorUsuario
+    }).map((response:Response)=>{
+      return response.json();
+    }).subscribe(
+      ()=> {console.log("Success");
+    },
+      (error)=>{
+        console.log('error');
+      }
+    )
+  }
 }
--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CreatePage } from '../create/create';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
+import { AuthSevice } from '../../services/auth/auth';
 
 @IonicPage()
 @Component({
@@ -8,8 +11,9 @@ import { CreatePage } from '../create/create';
   templateUrl: 'principal.html',
 })
 export class PrincipalPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  IdentificadorUsuario: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthSevice, public http:Http) {
+    this.IdentificadorUsuario = this.auth.idUsuario;
   }
 
   ionViewDidLoad() {
@@ -18,6 +22,19 @@ export class PrincipalPage {
 
   create(){
     this.navCtrl.push(CreatePage);
+    console.log(this.IdentificadorUsuario);
+    this.http.post('https://apex.oracle.com/pls/apex/indeme/INpolls/', {
+      'id': this.IdentificadorUsuario
+    }).map((response:Response)=>{
+      return response.json();
+    }).subscribe(
+      ()=> {console.log("Success");
+      
+    },
+      (error)=>{
+        console.log('error');
+      }
+    )
   }
  
 
