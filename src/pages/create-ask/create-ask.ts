8f81ab12ef3ecdf7d64fa7b67cf57fd182b0c83b
Ulises <ulisesmc1006@gmail.com>
2018-03-11 22:24:44 -0600

    Merge branch 'master' of https://github.com/INDEME/InvestigacionMercado

--- src/pages/create-ask/create-ask.ts
+++ src/pages/create-ask/create-ask.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
+import { AuthSevice } from '../../services/auth/auth';
 
 /**
  * Generated class for the CreateAskPage page.
@@ -15,12 +18,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreateAskPage {
   item;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  resultado: any;
+  id_encuesta: any;
+  id_tipo: any;
+  IdentificadorUsuario: any;
+  pregunta: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http, public auth: AuthSevice) {
+    this.IdentificadorUsuario = this.auth.idUsuario;
     this.item = navParams.data.item;
+    this.id_tipo = this.navParams.data.item.id;
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateAskPage');
   }
 
+  OnSave(){
+    this.http.get('https://apex.oracle.com/pls/apex/indeme/INpolls/' + this.IdentificadorUsuario +"/").map(res => res.json()).subscribe(data => {
+      this.resultado = data.items;
+      
+      if(data.items.length >= 1){
+        console.log("Holaaaaaaa: "+ this.resultado[data.items.length-1].id_encuesta);
+        this.id_encuesta = this.resultado[data.items.length-1].id_encuesta;
+      }
+
+      else{
+        console.log("id encuesta no encontrado"); 
+      }
+
+    });
+
+    this.http.post('https://apex.oracle.com/pls/apex/indeme/INask/', {
+      'id_encuesta': this.id_encuesta,
+      'id_tipo': this.id_tipo,
+      'pregunta': this.pregunta
+    }).map((response:Response)=>{
+      return response.json();
+    }).subscribe(
+      ()=> {console.log("Success");
+    },
+      (error)=>{
+        console.log('error');
+      }
+    )
+  }
 }
--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -19,75 +19,100 @@ import { CreateAskPage } from '../create-ask/create-ask';
   templateUrl: 'create.html',
 })
 export class CreatePage {
-
+  resultado: any;
   tipoPregunta = [];
   IdentificadorUsuario: any;
+  id_encuesta: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthSevice, public http:Http) {
     this.IdentificadorUsuario = this.auth.idUsuario;
+
+    this.http.get('https://apex.oracle.com/pls/apex/indeme/INpolls/').map(res => res.json()).subscribe(data => {
+      this.resultado = data.items;
+      
+      if(data.items.length >= 1){
+       console.log(this.resultado[0].id_encuesta);
+        this.id_encuesta = this.resultado[0].id_encuesta;
+      }
+      else{
+        console.log("Usuario y/o contraseña incorrectos."); 
+      }
+    });
+
     this.tipoPregunta = [
       {
         'id': '1',
         'title': 'Respuesta Única',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '2',
         'title': 'Respuesta Múltiple',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
+        
       },
       {
         'id': '5',
         'title': 'Evaluación',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '6',
         'title': 'Emoción',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '3',
         'title': 'Información',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '4',
         'title': 'Escala',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '7',
         'title': 'Abierta',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '23',
         'title': 'Slider de Cursor',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '21',
         'title': 'Respuesta Matriz',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
       {
         'id': '22',
         'title': '2 Dimensiones',
         'icon': '',
-        'color': '#0CA9EA'
+        'color': '#0CA9EA',
+        'id_encuesta': this.id_encuesta
       },
     ]
   }
-
+ 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreatePage');
     
@@ -99,7 +124,9 @@ export class CreatePage {
 
   OnSave(){
     this.http.post('https://apex.oracle.com/pls/apex/indeme/INpolls/', {
-      'id': this.IdentificadorUsuario
+      'id_encuesta': this.id_encuesta,
+      'id_tipo': this.tipoPregunta,
+      
     }).map((response:Response)=>{
       return response.json();
     }).subscribe(
