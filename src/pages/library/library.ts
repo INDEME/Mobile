e9e5bc84c5349f5ceca44c5dfbcc65da6e9364f5
Kevin Valencia Cruz <kev_val64@hotmail.com>
2018-06-04 14:02:01 -0500

    Buscador Libreria

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -8,6 +8,7 @@ import { Component, IonicPage, NavController, NavParams, LoadingController, Cons
 })
 export class LibraryPage {
   loading: any;
+  nombre: string;
   public list: any = [];
 
   constructor(public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams, public consulta: ConsultaProvider) {
@@ -18,6 +19,20 @@ export class LibraryPage {
   this.loading.present();
   }
 
+  search(nombre){
+    return new Promise(resolve => {
+      this.consulta.getListPreguntasByName(this.nombre).then(results => {
+        this.list = results;
+        this.loading.dismiss();
+        console.log(results);
+        return resolve();
+      }).catch(err => {  
+         console.log(err);  
+        return resolve();
+      });
+    })
+  }
+  
   loadList() {
     return new Promise(resolve => {
       this.consulta.getListPreguntas().then(results => {
--- src/providers/consulta/consulta.ts
+++ src/providers/consulta/consulta.ts
@@ -8,15 +8,24 @@ import { Http } from '@angular/http';
 */
 @Injectable()
 export class ConsultaProvider {
-
   constructor(public http: Http) {
     //console.log('Hello ConsultaProvider Provider');
   }
 
   public getListPreguntas() {
     return new Promise((resolve, reject) => {
-      this.http.get('https://apex.oracle.com/pls/apex/indeme/INlibrary/').map(res => res.json())
+      this.http.get('https://apex.oracle.com/pls/apex/indeme/IN/library/').map(res => res.json())
         .subscribe(data => {
+          resolve(data.items);
+        });
+    });
+  }
+
+  public getListPreguntasByName(nombre){
+    return new Promise((resolve, reject) => {
+      this.http.get('https://apex.oracle.com/pls/apex/indeme/IN/SearchLibrary/' + nombre).map(res => res.json())
+        .subscribe(data => {
+          console.log(data.items);
           resolve(data.items);
         });
     });
